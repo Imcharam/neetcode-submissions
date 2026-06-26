7b831da641be9e08384416e26bf8b01c5ee9a635
Imcharam <73675293+Imcharam@users.noreply.github.com>
2026-06-26 13:06:57 +0100

    Add: number-of-students-unable-to-eat-lunch - submission-2

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int counts[2] = {0,0};
+        for (int student : students) counts[student]++;
+
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) return counts[0] + counts[1];
+            counts[sandwich]--;
+        }
+        return 0;
+    }
+};
