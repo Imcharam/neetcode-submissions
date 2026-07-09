819fba545e92682c331ddf87cdb8debb5242852f
Imcharam <73675293+Imcharam@users.noreply.github.com>
2026-07-09 11:42:18 +0100

    Add: insertionSort - submission-11

---
+++ Data Structures & Algorithms/insertionSort/submission-11.cpp
@@ -0,0 +1,25 @@
+// Definition for a Pair
+// class Pair {
+// public:
+//     int key;
+//     string value;
+//
+//     Pair(int key, string value) : key(key), value(value) {}
+// };
+class Solution {
+public:
+    vector<vector<Pair>> insertionSort(vector<Pair>& pairs) {
+        int n = pairs.size();
+        vector<vector<Pair>> ans(n);
+        for (int i = 0; i < n; i++) {
+            int j = i - 1;
+            
+            while (j >= 0 && pairs[j+1].key < pairs[j].key) {
+                swap(pairs[j], pairs[j+1]);
+                j--;
+            }
+            ans[i] = pairs;
+        }
+        return ans;
+    }
+};
