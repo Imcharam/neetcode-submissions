28db3533e0931a3e8bc28d0bcddf0f72a24a80d7
Imcharam <73675293+Imcharam@users.noreply.github.com>
2026-07-16 15:28:49 +0100

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        if (points.size() <= 1 || points.size() == k) return points;
+        int pivot = points.size() - 1;
+        int start = 0;
+        kClosestHelper(points, k, start, pivot);
+        return vector<vector<int>>(points.begin(), points.begin()+k);
+    }
+
+private:
+    void kClosestHelper(vector<vector<int>>& points, int k, int start, int pivot) {
+        int j = start;
+        int distp = points[pivot][0]*points[pivot][0] + points[pivot][1]*points[pivot][1];
+        int disti;
+        
+        for (int i = start; i < pivot; i++) {
+            disti = points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            if (disti < distp) {
+                swap(points[i], points[j]);
+                j++;
+            }
+        }
+
+        swap(points[j], points[pivot]);
+        if (j == k-1) {
+            return;
+        } else if (j < k-1) {
+            kClosestHelper(points, k, j+1, pivot);
+        } else {
+            kClosestHelper(points, k, start, j-1);
+        }
+    }
+};
