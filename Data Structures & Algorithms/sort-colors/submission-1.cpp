15b28b06a38147b5b87c1b824ef78f3e43122eaa
Imcharam <73675293+Imcharam@users.noreply.github.com>
2026-08-05 09:22:09 +0100

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        map<int,int> colours;
+        for (int num : nums) {
+            colours[num]++;
+        }
+        
+        int j = 0;
+        for (int i = 0; i <= 3; i++) {
+            while (colours[i]-- > 0) {
+                nums[j++] = i;
+            }
+        }
+    }
+};
